b4eef9e4e29d2cf540d036237d1cd53f930da581
msanglikr <muneer@conviva.com>
2021-10-03 20:34:28 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/trbspace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trbspace.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:ECommerceApp/consts/theme_data.dart';
+import 'package:ECommerceApp/inner_screens/product_details.dart';
+import 'package:ECommerceApp/provider/dark_theme_provider.dart';
+import 'package:ECommerceApp/provider/products.dart';
+import 'package:ECommerceApp/screens/wishlist.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'inner_screens/brands_navigation_rail.dart';
+import 'inner_screens/categories_feeds.dart';
+import 'inner_screens/upload_product_form.dart';
+import 'provider/cart_provider.dart';
+import 'provider/favs_provider.dart';
+import 'screens/auth/login.dart';
+import 'screens/auth/sign_up.dart';
+import 'screens/bottom_bar.dart';
+import 'screens/cart.dart';
+import 'screens/feeds.dart';
+import 'screens/landing_page.dart';
+import 'screens/main_screen.dart';
+import 'screens/user_state.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreferences.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Object>(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('Error occured'),
+                ),
+              ),
+            );
+          }
+          return MultiProvider(
+              providers: [
+                ChangeNotifierProvider(create: (_) {
+                  return themeChangeProvider;
+                }),
+                ChangeNotifierProvider(
+                  create: (_) => Products(),
+                ),
+                ChangeNotifierProvider(
+                  create: (_) => CartProvider(),
+                ),
+                ChangeNotifierProvider(
+                  create: (_) => FavsProvider(),
+                ),
+              ],
+              child: Consumer<DarkThemeProvider>(
+                  builder: (context, themeData, child) {
+                return MaterialApp(
+                  title: 'Flutter Demo',
+                  theme:
+                      Styles.themeData(themeChangeProvider.darkTheme, context),
+                  home: UserState(),
+                  //initialRoute: '/',
+                  routes: {
+                    //   '/': (ctx) => LandingPage(),
+                    BrandNavigationRailScreen.routeName: (ctx) =>
+                        BrandNavigationRailScreen(),
+                    CartScreen.routeName: (ctx) => CartScreen(),
+                    Feeds.routeName: (ctx) => Feeds(),
+                    WishlistScreen.routeName: (ctx) => WishlistScreen(),
+                    ProductDetails.routeName: (ctx) => ProductDetails(),
+                    CategoriesFeedsScreen.routeName: (ctx) =>
+                        CategoriesFeedsScreen(),
+                    LoginScreen.routeName: (ctx) => LoginScreen(),
+                    SignUpScreen.routeName: (ctx) => SignUpScreen(),
+                    BottomBarScreen.routeName: (ctx) => BottomBarScreen(),
+                    UploadProductForm.routeName: (ctx) => UploadProductForm(),
+                  },
+                );
+              }));
+        });
+  }
+}
